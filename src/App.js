4b59c97819711cc6692d11c047ec74a4de3a50f4
JiinSeok <seokjiin1073@gmail.com>
2024-05-22 16:02:19 +0900

    feat: 포켓몬 정렬하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import mockItems from "./pokemons";
+
+// map으로 렌더링하기
+// sort로 정렬하기
+// filter로 삭제하기
+
+function Pokemon({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id);
+
+  return (
+    <div>
+      No.{item.id} {item.name}
+      <button onClick={handleDeleteClick}>삭제</button>
+    </div>
+  );
+}
 
 function App() {
+  const [items, setItems] = useState(mockItems);
+  const [direction, setDirection] = useState(1);
+
+  const handleAscClick = () => setDirection(1);
+  const handleDescClick = () => setDirection(-1);
+
+  const sortedItems = items.sort((a, b) => direction * (a.id - b.id));
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={handleAscClick}>도감번호 순서대로</button>
+        <button onClick={handleDescClick}>도감번호 반대로</button>
+      </div>
+      <ul>
+        {sortedItems.map((item) => (
+          <li key={item.id}>
+            <Pokemon item={item} onDelete={handleDelete} />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
